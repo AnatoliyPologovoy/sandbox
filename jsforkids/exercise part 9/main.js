6dbbb450085d178e1ea045fccb24449bb83d24c1
Anatoliy Pologovoy <pologovoy89@mail.ru>
2023-02-13 15:43:34 +0300

    add solution tasks jsForKids part 9 #1-2

---
+++ jsforkids/exercise part 9/main.js
@@ -0,0 +1,18 @@
+
+$('h1').text('Мои друзья');
+let friends = ['Костян', 'Андрей', 'Роман'];
+let body = $('body');
+/*
+friends.forEach((name) => {
+    body.append('<p>' + name +'</p>');
+    //p.hide().fadeIn(1000);
+    console.log($('p').text())
+});*/
+
+for (let i = 1; i < 6; i++) {
+    $('h1').fadeOut(1000).fadeIn(1000);
+
+}
+//$('p').hide().fadeIn(1000);
+//$('p')
+
